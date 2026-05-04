0b3a6d39b892cbfd9ec84364152e1b5c18801cc9
Enrique-290 <enri290@gmail.com>
2026-05-03 19:04:25 -0600

    Add files via upload

--- modules/pagina/pagina.js
+++ modules/pagina/pagina.js
@@ -1,1 +1,8 @@
-// JS módulo pagina
+(function(){
+  const btn = document.getElementById("pagina-go-3");
+  if(!btn) return;
+  btn.addEventListener("click", ()=>{
+    const nav = document.querySelector('#menu button[data-module="pagina3"]');
+    if(nav) nav.click();
+  });
+})();
